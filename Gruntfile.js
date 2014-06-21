6340085e89ae423b6cfe7cfd3d669b32a21772dc
Sam Desborough <sam.desborough@gmail.com>
2014-06-21 11:30:55 +0100

    Added gruntfile and initial node packages

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+'use strict';
+
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    express: {
+      dev: {
+        options: {
+          script: 'app.js'
+        }
+      },
+    },
+
+    sass: {
+      dist: {
+        files: {
+          'assets/css/styles.css': 'assets/sass/styles.scss'
+        }
+      },
+      dev: {
+        options: {
+          sourceMap: true
+        },
+        files: {
+          'assets/css/styles.css': 'assets/sass/styles.scss'
+        }
+      }
+    },
+
+    watch: {
+      sass: {
+        files: ['assets/sass/*.scss'],
+        tasks: ['sass:dev'],
+        options: {
+          spawn: false
+        },
+      },
+      express: {
+        files: ['app/**/*.js'],
+        tasks: ['express:dev'],
+        options: {
+          spawn: false
+        }
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-express-server');
+  grunt.loadNpmTasks('grunt-sass');
+
+  grunt.registerTask('serve', ['express:dev', 'sass:dev', 'watch']);
+};
