78337559189881815a38d4ad769b0fe56478cc72
khamiltonuk <krisham87@hotmail.com>
2014-06-21 12:28:26 +0100

    about overlay introduction

--- assets/js/init.js
+++ assets/js/init.js
@@ -8,6 +8,11 @@ $(function ($) {
 		
 	});
 
+	$(".close").click(function(e){
+		e.preventDefault();
+		$(".overlay").addClass('hidden');
+	})
+
     $(".spin-plus").click(function () {
         $(this).toggleClass('animate');
         $("header").toggleClass('shown-sub');
@@ -16,10 +21,12 @@ $(function ($) {
     enquire.register("screen and (min-width: 767px)", {
 	    match : function() {
 	    	$("header").addClass('shown-sub');
+	    	$(".about-text").addClass('center');
 	    },
 	    unmatch : function() {
 	    	$(".spin-plus").removeClass('animate');
 	    	$("header").removeClass('shown-sub');
+	    	$(".about-text").removeClass('center');
 	    },
 	});
 });
--- pages/footer.php
+++ pages/footer.php
@@ -1,12 +1,16 @@
 <footer>
 	<h1>footer</h1>
 </footer>
-<div class="overlay hidden">
+<div class="overlay">
+	<button class="close">Close</button>
 	<div class="about-text">
-		<h2 class="hidden-text">About me</h2> 
-		<p>Michael Zoidis is a multi-disciplined designer, unravelling a career in visual and user-experience design. I strive for better communication and engaging interaction through graphic design, video and sound. Always learning, exploring and drawing inspiration from a wide variety of areas, I strive to bring together experimental, playful and immersive experiences that absorb all the senses.</p>
-		<h2>Clients Include;</h2>
-		<p>Penguin, Erased Tapes, Kurt Geiger, Studio Moross, Signature Pictures, Victoria &amp; Albert Museum, VideoJug, Farm Group TV
+		<div class="relative-wrapper">
+			<h2 class="hidden-text">About me</h2> 
+			<p>Michael Zoidis is a multi-disciplined designer, unravelling a career in visual and user-experience design. I strive for better communication and engaging interaction through graphic design, video and sound. Always learning, exploring and drawing inspiration from a wide variety of areas, I strive to bring together experimental, playful and immersive experiences that absorb all the senses.</p>
+			<h2>Clients Include;</h2>
+			<p>Penguin, Erased Tapes, Kurt Geiger, Studio Moross, Signature Pictures, Victoria &amp; Albert Museum, VideoJug, Farm Group TV	</p>	
+			<img class="mikes-head" src="assets/images/myhead.jpg" alt="" />
+		</div>
 	</div>
 </div>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
