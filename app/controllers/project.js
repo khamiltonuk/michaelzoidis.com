72deb7a32c894d316289d690537eeb4a19973c46
Sam Desborough <sam.desborough@gmail.com>
2014-07-02 23:13:01 +0100

    Use markdown rendering for project pages

--- app/controllers/project.js
+++ app/controllers/project.js
@@ -1,27 +1,27 @@
 'use strict';
 
 var fs = require('fs'),
-    path = __dirname + '/../../data/projects/';
+    marked = require('marked'),
+    path = __dirname + '/../../data/projects/',
+    extension = '.md';
 
 /**
- * render a project page
+ * render a project page: attempt to load a markdwon file matching the 
+ * project path from the url, render it to html using the marked library
+ * and pass it to the template.
+ *
+ * @see https://github.com/chjj/marked
  */
 module.exports.render = function (req, res) {
-  var file = req.params.project + '.json';
+  var file = path + req.params.project + extension;
 
-  fs.readFile(path + file, function (error, data) {
+  fs.readFile(file, { encoding: 'utf8' }, function (error, data) {
+    console.log(data);
     if (error) {
-      console.log(error);
       res.status(404);
       res.render('pages/404', { name: 'Page not found' });
     } else {
-      data = JSON.parse(data);
-      res.render('pages/project', 
-        { 
-          name: data.name,
-          project: data
-        }
-      );
+      res.render('pages/project', { html: marked(data) });
     }
   });
 };
