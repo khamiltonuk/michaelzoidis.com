e45aee9011bf67fcfd77f5d91408c419c013241b
khamiltonuk <krisham87@hotmail.com>
2014-07-02 20:42:51 +0100

    remove hidden image in scroll up svg, introduce svg links and fallbacks, scroll to top now works

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,6 +1,40 @@
-//
-
 $(function ($) {
+	if(document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image", "1.1")){
+		(function(H){H.className=H.className.replace(/\bno-svg\b/,'svg')})(document.documentElement)
+	}else{
+		
+	}
+
+	var windowHeight = $(window).height();
+	var baseSpace = windowHeight / 10;
+	disfromtop = $(window).scrollTop();
+	var spaceOutLinks = function(){
+		$('.right-floating-nav object').each(function(){
+			if (disfromtop < 1000){
+				$(this).css({
+				 marginBottom: (baseSpace + (disfromtop/20)) + 'px'
+				});
+			}
+		});
+	}
+	spaceOutLinks();
+
+	$(window).scroll(function() {
+		disfromtop = $(this).scrollTop();
+		disfromtop > 250 ? $('#go-top').addClass('animate') : $('#go-top').removeClass('animate') ;
+
+		// space out links
+		spaceOutLinks();
+	});
+
+	$('#go-top').click(function(e){
+		e.preventDefault();
+		$('html, body').stop().animate({
+			scrollTop: 0
+		});
+	});
+	
+
 	$(".js-about").click(function(e){
 		e.preventDefault();
 		console.log("working")
